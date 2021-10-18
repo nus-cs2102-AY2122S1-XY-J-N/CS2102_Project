0f8f5a25aab4c4015f36c773ce578b4533724f2f
BigQuery AutoFormatter <test@example.com>
2021-10-18 10:04:46 +0000

    Auto-format queries

--- crappy.sql
+++ crappy.sql
@@ -1,29 +1,16 @@
-DROP
-TABLE 
-IF 
-EXISTS
-Test;
-
-CREATE
-TABLE
-Test (a INT, b 
-INT);
-
-INSERT 
-INTO
-Test VALUES( (1,2),  
-
-
-
-(3,4) );
-
+DROP TABLE IF EXISTS Test;
+CREATE TABLE Test
+(
+  a INT,
+  b INT
+);
+INSERT INTO Test
+VALUES
+  ((1, 2), (3, 4));
 SELECT
-*
-
-
-
+  *
 FROM
-
-Test WHERE a  < b;
-
+  Test
+WHERE
+  a < b;
 DROP TABLE IF EXISTS Test;
